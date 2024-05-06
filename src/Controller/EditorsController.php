08a296afd481d3bb2d0e70bb41fe6bb1f0e614af
DuncanCos <foxlolmen99@gmail.com>
2024-05-06 12:39:16 +0200

    splitting metadata into multiple controller

--- src/Controller/EditorsController.php
+++ src/Controller/EditorsController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\NEditors;
+use App\Form\EditorsFormType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,4 +21,46 @@ class EditorsController extends AbstractController
             'controller_name' => 'EditorsController',
         ]);
     }
+
+    #[Route('/editors/create', name: 'app_editors_create')]
+    public function create(EntityManagerInterface $em, Request $request): Response
+    {
+
+        $editors = new NEditors();
+
+        $form = $this->createForm(EditorsFormType::class, $editors);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $editors = $form->getData();
+            $em->persist($editors);
+            $em->flush();
+            return $this->redirectToRoute('app_game');
+        }
+
+        return $this->render('create.html.twig', [
+            'controller_name' => 'Add a Game Editor',
+            'form' => $form,
+        ]);
+
+    }
+
+    #[Route('/editors/delete/{id}', name: 'app_editors_delete')]
+    public function delete(EntityManagerInterface $em, int $id): Response
+    {
+        $editor = $em->getRepository(NEditors::class)->find($id);
+
+        if(!$editor) {
+            throw $this->createNotFoundException(
+                'No platform found'
+            );
+        }
+
+        $em->remove($editor);
+        $em->flush();
+
+        return $this->redirectToRoute('app_game');
+    }
+
 }
--- src/Controller/PlateformsController.php
+++ src/Controller/PlateformsController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\NPlateforms;
+use App\Form\PlateformsFormType; 
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,5 +20,46 @@ class PlateformsController extends AbstractController
         return $this->render('plateforms/index.html.twig', [
             'controller_name' => 'PlateformsController',
         ]);
+    }
+
+    #[Route('/plateforms/create', name: 'app_plateforms_create')]
+    public function create(EntityManagerInterface $em, Request $request): Response
+    {
+
+        $plats = new NPlateforms();
+
+        $form = $this->createForm(PlateformsFormType::class, $plats);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $plats = $form->getData();
+            $em->persist($plats);
+            $em->flush();
+            return $this->redirectToRoute('app_game');
+        }
+
+        return $this->render('create.html.twig', [
+            'controller_name' => 'Add a plateform',
+            'form' => $form,
+        ]);
+
+    }
+
+    #[Route('/plateforms/delete/{id}', name: 'app_plateforms_delete')]
+    public function delete(EntityManagerInterface $em, int $id): Response
+    {
+        $platforms = $em->getRepository(NPlateforms::class)->find($id);
+
+        if(!$platforms) {
+            throw $this->createNotFoundException(
+                'No platform found'
+            );
+        }
+
+        $em->remove($platforms);
+        $em->flush();
+
+        return $this->redirectToRoute('app_game');
     }
 }
