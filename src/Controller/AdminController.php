5a1238fc1aaadd10aa2b49a2b08cac6bcfcb84e1
methiathibault <methiathibault@gmail.com>
2024-05-06 11:06:23 +0200

    admin page finished

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use App\Entity\Offers;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $games = $em->getRepository(Game::class)->findAll();
+        $offers = $em->getRepository(Offers::class)->findAll();
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'games' => $games,
+            'offers' => $offers,
         ]);
     }
 }
